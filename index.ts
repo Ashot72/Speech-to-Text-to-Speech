34482049c13b5e87f579837bae6c60dcb5cb686f
giangdh142536 <146085468+giangdh142536@users.noreply.github.com>
2023-11-01 16:31:48 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -58,7 +58,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 io.on('connection', (socket) => {
   console.log('user connected');
